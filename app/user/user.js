e47125bf64b8f01e1b67a0a8b42b864f209d088f
Yoan Ficadiere <yficadiere@inbox.fr>
2017-10-06 12:34:59 +0200

    First commit

---
+++ app/user/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  userName: String,
+  email: String,
+  password: String
+});
+
+mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model('User');
